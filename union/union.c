c48c7b3b0fd2d007256c8a28d67069ee51145473
BarbaraC <55756205+BarbaraC12@users.noreply.github.com>
2021-06-14 18:00:22 +0200

    union exam OK

--- union/union.c
+++ union/union.c
@@ -1,20 +1,15 @@
 #include <unistd.h>
 #define MAX 255
-int	main(int argc, char **argv)
-{
-	int	tab[MAX] = {0};
-	int	i, j = 0;
+int		main(int argc, char **argv) {
+	int		tab[MAX] = {0};
+	int		i, j = 0;
 
-	if (argc == 3)
-	{
+	if (argc == 3) {
 		i = 1
-		while (i < 3)
-		{
+		while (i < 3) {
 			j = 0;
-			while (argv[i][j])
-			{
-				if (!tab[(unsigned char)argv[i][j]])
-				{
+			while (argv[i][j]) {
+				if (!tab[(unsigned char)argv[i][j]]) {
 					tab[(unsigned char)argv[i][j]] = 1;
 					write(1, &argv[i][j], 1);
 				}
