3325a8a2ff7b5ce1e0d4998b176fd5916e072181
BarbaraC <55756205+BarbaraC12@users.noreply.github.com>
2021-06-14 17:59:17 +0200

    inter exam OK

--- inter/inter.c
+++ inter/inter.c
@@ -1,23 +1,18 @@
 #include <unistd.h>
 #define MAX 255
-int
-	main(int argc, char **argv)
-{
+
+int		main(int argc, char **argv) {
 	int	tab[MAX] = {0};
 	int	i, j = 0;
 
-	if (argc == 3)
-	{
+	if (argc == 3) {
 		i = 2;
-		while (i > 0)
-		{
+		while (i > 0) {
 			j = 0;
-			while (argv[i][j])
-			{
+			while (argv[i][j]) {
 				if (i == 2 && !tab[(unsigned char)argv[i][j]])
 					tab[(unsigned char)argv[i][j]] = 1;
-				else if (i == 1 && tab[(unsigned char)argv[i][j]] == 1)
-				{
+				else if (i == 1 && tab[(unsigned char)argv[i][j]] == 1) {
 					write(1, &argv[i][j], 1);
 					tab[(unsigned char)argv[i][j]] = 2;
 				}
